6e18b455c17f9b5f4d2abb79fed987d83519f6aa
killianLC <killianlecorre18@gmail.com>
2021-11-19 15:37:54 +0100

    first commit

---
+++ SafeTravel/src/test/java/com/sf/SafeTravel/SafeTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sf.SafeTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
