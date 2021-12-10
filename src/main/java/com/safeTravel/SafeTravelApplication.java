59998a0c2f75e767ab71667bf5172f31d8a0b5b4
Pierre Le Brun <pierreyaudet@gmail.com>
2021-12-10 15:22:43 +0100

    chore(nouv projet)

---
+++ src/main/java/com/safeTravel/SafeTravelApplication.java
@@ -0,0 +1,13 @@
+package com.safeTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safeTravel/SafeTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safeTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
