24213687de201ea21b3aebf4d2a6ee4ab9bd4d0a
killianLC <killianlecorre18@gmail.com>
2021-12-17 15:37:04 +0100

    userController

--- src/main/java/com/safeTravel/SafeTravelApplication.java
+++ src/main/java/com/safeTravel/SafeTravelApplication.java
@@ -2,8 +2,12 @@ package com.safeTravel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.safeTravel"
+})
 public class SafeTravelApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/safeTravel/entity/User.java
@@ -0,0 +1,49 @@
+package com.safeTravel.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "prenom", nullable = false)
+    private String prenom;
+
+    @Column(name = "nom", nullable = false)
+    private String nom;
+
+    @Column(name = "email", nullable = false)
+    private Boolean email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
