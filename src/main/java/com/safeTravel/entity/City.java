54e605dc14cacf46ccab6827140ea58e3cb5de3f
Pierre Le Brun <pierreyaudet@gmail.com>
2022-02-10 16:58:12 +0100

    oui

--- src/main/java/com/safeTravel/entity/City.java
+++ src/main/java/com/safeTravel/entity/City.java
@@ -23,5 +23,5 @@ public class City {
     private Set<Comment> comments;
 
     @OneToMany(mappedBy = "city", fetch = FetchType.LAZY)
-    private Set<Statistic> statistics;
+    private Set<Note> notes;
 }
