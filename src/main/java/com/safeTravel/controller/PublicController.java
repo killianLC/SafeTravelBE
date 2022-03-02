6117b9e68775ec49a5adba367e97cf1b35a76665
Yoann <contactyoannmc@gmail.com>
2022-03-02 18:24:32 +0100

    Ajout - Public Cities

--- src/main/java/com/safeTravel/controller/PublicController.java
+++ src/main/java/com/safeTravel/controller/PublicController.java
@@ -1,7 +1,10 @@
 package com.safeTravel.controller;
 
 import com.safeTravel.dto.CityClassementDto;
+import com.safeTravel.dto.ReducedCityDto;
 import com.safeTravel.service.CityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,8 @@ import java.util.List;
 @RequestMapping({"/public"})
 public class PublicController {
 
+    private static final Logger logger = LoggerFactory.getLogger(PublicController.class);
+
     @Autowired
     private CityService cityService;
 
@@ -22,5 +27,18 @@ public class PublicController {
     @ResponseStatus(HttpStatus.OK)
     public List<CityClassementDto> getTop10OrderByDesc() {
         return cityService.getTop10ByNotesDesc();
+    }
+
+    /**
+     * Endpoint /cities type GET
+     *
+     * @return List<CityDto> of the cities contains in database
+     */
+    @GetMapping({"/cities"})
+    @ResponseStatus(HttpStatus.OK)
+    public List<ReducedCityDto> getAll() {
+        List<ReducedCityDto> cities = cityService.getAllReducedCity();
+        logger.info("City, getAll() :{}", cities);
+        return cities;
     }
 }
